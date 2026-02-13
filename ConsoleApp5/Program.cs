cacfcc7dbe7bf354ebf923be7ab24a50563a2631
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 22:11:14 -0400

    tarea5

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        int mayor = int.MinValue; // Valor inicial muy pequeño
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > mayor)
+            {
+                mayor = num;
+            }
+        }
+
+        Console.WriteLine("El número mayor es: " + mayor);
+        Console.ReadLine();
+    }
+}

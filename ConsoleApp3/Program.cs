abda0dbed4156a62a386a70901add6a58a62ee69
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 21:43:19 -0400

    tarea3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double suma = 0;
+        double num;
+
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            num = Convert.ToDouble(Console.ReadLine());
+            suma += num;
+        }
+
+        
+        double media = suma / 5;
+
+        Console.WriteLine("La media aritmética es: " + media);
+        Console.ReadLine();
+    }
+}

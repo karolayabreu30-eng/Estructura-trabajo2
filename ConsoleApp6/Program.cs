9da3db5d9c23de3db3b7af934f16ea16bd3dd7e8
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 22:31:32 -0400

    tarea6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("¿Cuántos números desea ingresar?: ");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        int num;
+        int contadorPrimos = 0;
+
+        for (int i = 1; i <= cantidad; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (EsPrimo(num))
+            {
+                contadorPrimos++;
+            }
+        }
+
+        Console.WriteLine("Cantidad de números primos: " + contadorPrimos);
+        Console.ReadLine();
+    }
+
+    static bool EsPrimo(int n)
+    {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}

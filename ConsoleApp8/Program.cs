9ff91b739e4f2ab42c1a04d66ed22a7ce3d4e96c
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 22:57:53 -0400

    tarea8

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Ingrese el número " + i + ": ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num > 0)
+            {
+                positivos++;
+            }
+            else if (num < 0)
+            {
+                negativos++;
+            }
+            else
+            {
+                ceros++;
+            }
+        }
+
+        Console.WriteLine("\nResultados:");
+        Console.WriteLine("Positivos: " + positivos);
+        Console.WriteLine("Negativos: " + negativos);
+        Console.WriteLine("Ceros: " + ceros);
+
+        Console.ReadKey();
+    }
+}

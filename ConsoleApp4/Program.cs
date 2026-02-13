452abd71ea3c63ad46b957568ab657f81958ecfc
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 21:58:13 -0400

    tarea4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int mayor = Math.Max(num1, num2);
+        int mcm = mayor;
+              
+        while (true)
+        {
+            if (mcm % num1 == 0 && mcm % num2 == 0)
+            {
+                break;
+            }
+            mcm += mayor;
+        }
+
+        Console.WriteLine("El Mínimo Común Múltiplo (MCM) es: " + mcm);
+        Console.ReadLine();
+    }
+}

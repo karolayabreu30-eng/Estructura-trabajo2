0bcaf55164905c6554ee8e4038c8846a5293a5d9
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-02-12 20:51:30 -0400

    Tarea2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite un número: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int suma = 0;
+        int i = 1;
+
+        while (i < num)
+        {
+            if (num % i == 0)
+            {
+                suma += i;
+            }
+            i++;
+        }
+
+        if (suma == num)
+        {
+            Console.WriteLine("El número " + num + " es un número perfecto.");
+        }
+        else
+        {
+            Console.WriteLine("El número " + num + " no es un número perfecto.");
+        }
+
+        Console.ReadKey();
+    }
+}
